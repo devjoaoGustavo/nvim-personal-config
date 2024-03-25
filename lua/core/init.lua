570851898195ce67bd4246a4719807bdafa01374
João Gustavo de Paula <dev.joaogustavo@gmail.com>
2024-03-25 16:39:37 -0300

    remove coc

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,7 +1,6 @@
 require('core.diagnostic_config')
 require('core.keymaps_config')
 require('core.autocmd')
-require('core.coc')
 
 vim.keymap.set("n", "<leader>n", vim.cmd.NvimTreeFindFileToggle,
   { noremap = true, silent = true })
